fd7cf955738fe39d0b187073235f36101b34d18c
Yulany Munevar <andra_852@hotmail.com>
2023-05-16 20:33:31 -0500

    routes definition

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,15 @@
 import { Application } from 'express';
+import milhoja from './api/milhojas';
+import user from './api/users';
+import customer from './api/customers';
+
+// import upload from './api/upload';
+
 
 function routes(app:Application) {
-
+  app.use('/api/customer', customer);
+  app.use('/api/milhojas',milhoja);
+  app.use('/api/user', user);
 
 }
 
